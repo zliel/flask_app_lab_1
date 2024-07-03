1d85e8d3c02ff3a32240f6fd0637d507b64f2a64
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-03 14:11:06 -0700

    Initial Commit

---
+++ flask_lab_1.py
@@ -0,0 +1,18 @@
+# hello_flask.py
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+# create an instance of Flask
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+# route decorator binds a function to a URL
+@app.route('/')
+def hello():
+  return ('<p>Oscar A.: Iykyk</p>'
+          '<p>Claire L.: Idk</p>'
+          '<p>Kate S.: Tbh wdym</p>')
+
+@app.route('/zackary')
+def zackary():
+    return render_template('template.html', acronym="Tfti", name='Zackary L.')
